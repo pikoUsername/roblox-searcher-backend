27513a09051950ad4baf2e70dfc566098802bc38
pikoUsername <galymzhan.amantaj@gmail.com>
2024-09-24 17:33:01 +0500

    Huh

--- app/web/routes.py
+++ app/web/routes.py
@@ -278,7 +278,7 @@ async def robux_amount(
 	if not user_id:
 		logger.info('Starting to execute a plans')
 		driver_requests.get("https://roblox.com")
-		raw_response = driver_requests.execute_script("localStorage.getItem('PresenceData');")
+		raw_response = driver_requests.execute_script("return localStorage.getItem('PresenceData');")
 		logger.info(f"Raw response: {raw_response}")
 		response = json.loads(raw_response['value'])
 		if not response:
